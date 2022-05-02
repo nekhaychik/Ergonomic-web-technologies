19577c362922b4aa14c9f6577c899690f7251593
nekhaychik <irina.nekhaychik.i@gmail.com>
2022-05-02 11:04:28 +0300

    add php

--- form.php
+++ form.php
@@ -1,4 +1,13 @@
 <?php
-echo 12;
-
-?>
+ 
+if($_GET['action'] == 'add') {
+    $handle = fopen('text.txt', 'a');
+    $line = $_POST['name'] . ',' . $_POST['email'] . "\n";
+    file_put_contents('text.txt', $line, FILE_APPEND);
+    echo 'Строка добавлена в файл.';
+} else if($_GET['action'] == 'clear') {
+    file_put_contents('text.txt', '');
+    echo 'Файл очищен!';
+} else if($_GET['action'] == 'show') {
+    echo str_replace("\n", '<br>', file_get_contents('text.txt'));
+}
