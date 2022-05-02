b3d0fe5f2dc67702f94634dda42f6182615cf4b2
nekhaychik <irina.nekhaychik.i@gmail.com>
2022-05-02 10:52:42 +0300

    add email vaidetion

---
+++ script.js
@@ -0,0 +1,27 @@
+function start () {
+  $(".btn-submit").on("click", validate);
+
+  function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  function sendForm() {
+    $(".error").text("Form sending").fadeIn();
+  }
+
+  function validate() {
+    var email = $(".email").val();
+    var $error = $(".error");
+    $error.text("");
+
+    if (validateEmail(email)) {
+      $error.fadeOut();
+      sendForm();
+    } else {
+      $error.fadeIn();
+      $error.text(email + " is not valid");
+    }
+    return false;
+  }
+}
